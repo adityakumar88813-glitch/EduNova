0445b82754a2beba72095072bf455cc7d2178d78
Aditya Kumar <adityakumar88813@gmail.com>
2026-08-05 01:16:18 +0530

    SignUP and Login button

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,8 +4,16 @@ import { Link, matchPath } from 'react-router-dom'
 import logo from "../../assets/Logo-Full-Light.png"
 import{NavbarLinks} from "../../data/navbarlink"
 import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import Button from '../core/HomePage/Button'
+import ProfileDropDown from '../core/Auth/ProfileDropDown'
+
 
 const Navbar = () => {
+  const{token} = useSelector((state)=>state.auth);
+  const{user} = useSelector((state)=>state.profile);
+  const{totalItems} = useSelector((state)=>state.cart);
 
    
     const location = useLocation();
@@ -45,8 +53,56 @@ const Navbar = () => {
   </nav>
 
 
-{/* LOGIN SIGNUP BUTTON */}
+{/* LOGIN SIGNUP BUTTON/dashboard*/}
+<div className='flex gap-x-4 items-center'>
 
+{ //styling karna hai 
+  user && user?.accountType != "Instructor" &&(
+    <Link to="/dashboard/cart" className='relative'>
+           <AiOutlineShoppingCart />
+           {
+            totalItems > 0 && (
+              <span>
+                {totalItems}
+              </span>
+            )
+           }
+    </Link>
+  )
+}
+
+{
+  token===null && (
+    <Link to="/login">
+      <button className='border border-richblack-700 bg-richblack-800  px-[12px] py-[8px]
+          text-richblack-100 rounded-md' >
+        Login
+      </button>
+    </Link>
+  )
+}
+
+
+{
+  token===null && (
+    <Link to="/signup">
+      <button className='border border-richblack-700 bg-richblack-800  px-[12px] py-[8px]
+          text-richblack-100 rounded-md'>
+        Signup
+      </button>
+    </Link>
+  )
+}
+
+
+//user hai to sab dikhana hai like cart
+{
+  token !== null && <ProfileDropDown/>
+}
+
+
+
+</div>
 
 
 
